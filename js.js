3679babb56cd77677aae317e94d116d4b781d062
Dolore-Ipsum <67329384+Dolore-Ipsum@users.noreply.github.com>
2020-12-14 23:43:29 +0300

    New version with tabs

--- js.js
+++ js.js
@@ -1,15 +1,33 @@
 //  Аккордеон и табы
+document.addEventListener('DOMContentLoaded', function(){
 
-  $( function() {
-  $( "#tabs" ).tabs();
- } );
+
+//   $( function() {
+//   $( "#tabs" ).tabs();
+//  } );
 
 
  $( function() {
   $( ".accordion" ).accordion();
 } );
 
-$( ".accordion" ).accordion("refresh");
+// $( ".accordion" ).accordion("refresh");
 
+document.querySelector('.tab-list').addEventListener('click', function(){
+  document.querySelector('.accordeon-block').classList.add('accordeon-active')
+})
+
+
+document.querySelectorAll('.tabs__btn').forEach(function(tabsBtn){
+ tabsBtn.addEventListener('click', function(event) {
+  const path = event.currentTarget.dataset.path
+
+  document.querySelectorAll('.tab-content').forEach(function(tabContent){
+    tabContent.classList.remove('tab-content-active')
+  })
+  document.querySelector(`[data-target="${path}"]`).classList.add('tab-content-active')
+ })
+})
+})
 
 
